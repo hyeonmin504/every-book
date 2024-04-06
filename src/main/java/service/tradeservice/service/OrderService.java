e3d21c5f0fc88c483488f0f84f7547b1280ca03d
hyunmin504 <hyunmin504@naver.com>
2024-04-07 00:11:56 +0900

    아이템 삭제 검증

--- src/main/java/service/tradeservice/service/OrderService.java
+++ src/main/java/service/tradeservice/service/OrderService.java
@@ -3,13 +3,13 @@ package service.tradeservice.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import service.tradeservice.domain.Orders;
 import service.tradeservice.repository.OrderRepository;
 import service.tradeservice.repository.impl.OrderRepositoryCustom;
 
 @Service
 @RequiredArgsConstructor
 public class OrderService {
-//test test
     @Autowired
     OrderRepository orderRepository;
 
